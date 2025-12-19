b089e361804a545f3fab6f2ac85b0ac6cdcd69df
okas832 <okas832@kaist.ac.kr>
2025-12-19 11:24:36 +0900

    Fix

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Research from '../views/Research.vue'
 import Professor from '../views/Professor.vue'
@@ -52,7 +52,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
